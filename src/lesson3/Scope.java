25886108b4f78fe34265f31dee84a80803aded0b
Mkhitar <www.mkhitaryan94@mail.ru>
2021-10-09 22:24:22 +0500

    add lesson3

--- src/lesson3/Scope.java
+++ src/lesson3/Scope.java
@@ -5,10 +5,8 @@ public class Scope {
       int x;
       x=10;
       if (x == 10)
-          int y= 20;
-        System.out.println("x and y: " + x + " " + y);
-        x = y * 2;
-        System.out.println("x ravno " + x);
+
+        System.out.println("x and y: " + x );
 
     }
 }
